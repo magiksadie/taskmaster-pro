f827966c4314b9a0e2e5676743a1c5840360965c
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-03-30 19:14:02 -0500

    funcitoning date edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,6 +61,22 @@ $(".list-group").on("blur", "textarea", function() {
   var taskP = $("<p>").addClass("m-1").text(text);
   $(this).replaceWith(taskP);
 });
+//due date was clicked
+$(".list-group").on("click", "span", function() {
+  var date = $(this).text().trim();
+  var dateInput = $("<input>").val(date).attr("type", "text").addClass("form-control");
+  $(this).replaceWith(dateInput);
+  dateInput.trigger("focus");
+});
+$(".list-group").on("blur", "input", function() {
+  var date = $(this).val().trim();
+  var status = $(this).closest(".list-group").attr("id").replace("list-", "");
+  var index = $(this).closest(".list-group-item").index();
+  tasks[status][index].date = date;
+  saveTasks();
+  var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(date);
+  $(this).replaceWith(taskSpan);
+});
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
